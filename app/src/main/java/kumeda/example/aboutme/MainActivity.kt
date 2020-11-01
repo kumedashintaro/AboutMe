8eb35b76a4c367ab3895f268474ae03ce4e4ad8b
Kumeda Shintaro <pii.k2.bys@gmail.com>
2020-11-02 06:58:45 +0900

    コメント追記

--- app/src/main/java/kumeda/example/aboutme/MainActivity.kt
+++ app/src/main/java/kumeda/example/aboutme/MainActivity.kt
@@ -8,34 +8,38 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //DONEボタンがクリックされた時
         findViewById<Button>(R.id.done_button).setOnClickListener{
             addNickname(it)
 
         }
 
-        findViewById<TextView>(R.id.nickname_taext).setOnClickListener{
+        //表示されているニックネームがクリックされた時
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener{
             updateNickname(it)
         }
-
     }
 
     private fun addNickname(view: View){
         val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_taext)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
+        //入力したニックネームを表示するニックネームへいれる
         nicknameTextView.text = editText.text
+        //ニックネーム入力蘭を消す
         editText.visibility = View.GONE
+        //DONEボタンを消す
         view.visibility = View.GONE
+        //ニックネームを表示する
         nicknameTextView.visibility = View.VISIBLE
 
-        // Hide the keyboard.
+        // キーボードを隠す
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
@@ -44,19 +48,18 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val doneButton = findViewById<Button>(R.id.done_button)
 
+        //ニックネーム入力蘭を表示
         editText.visibility = View.VISIBLE
+        //DONEボタンを表示
         doneButton.visibility = View.VISIBLE
+        //表示されていたニックネームを消す
         view.visibility = View.GONE
 
-        // Set the focus to the edit text.
+        // ニックネーム入力蘭にフォーカスを変える
         editText.requestFocus()
 
-        // Show the keyboard.
+        // キーボードを表示する
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(editText, 0)
     }
-
-
-
-
 }
